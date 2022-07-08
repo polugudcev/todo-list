c3fd8b9b863bf574261305512273cb57148e12bb
Azrail07 <avpownedyou@gmail.com>
2022-07-08 17:10:03 +0300

    change structure

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let allTasks = [];
-let valueInput = "";
-const activatedEditTask = null;
 const URL = "http://localhost:8000";
-let newEdittask = "";
+let newEditTask = "";
 
 window.onload = async () => {
   const input = document.getElementById("input-todo");
@@ -15,118 +13,51 @@ window.onload = async () => {
 };
 
 const fetchAllTasks = async () => {
-  const resp = await fetch(`${URL}/allTasks`, {
-    method: "GET",
-    headers: { "Content-type": "application/json;charset=utf-8" },
-  });
-  const result = await resp.json();
-  return result;
+  try {
+    const resp = await fetch(`${URL}/allTasks`, {
+      method: "GET",
+      headers: { "Content-type": "application/json;charset=utf-8" },
+    });
+
+    const result = await resp.json();
+    return result;
+  } catch (error) {
+    alert("Error");
+  }
 };
 
 const addNewTask = async () => {
-  const input = document.getElementById("input-todo");
-  if (valueInput.trim() === "") {
-    return;
+  try {
+    const input = document.getElementById("input-todo");
+    if (input.value.trim() === "") {
+      return;
+    }
+
+    const resp = await fetch(`${URL}/createTask`, {
+      method: "POST",
+      headers: { "Content-type": "application/json;charset=utf-8" },
+      body: JSON.stringify({
+        text: input.value,
+        isCheck: false,
+      }),
+    });
+
+    const result = await resp.json();
+    allTasks.push(result);
+    input.value = "";
+    render();
+  } catch (error) {
+    alert("Error");
   }
-
-  const resp = await fetch(`${URL}/createTask`, {
-    method: "POST",
-    headers: { "Content-type": "application/json;charset=utf-8" },
-    body: JSON.stringify({
-      text: valueInput,
-      isCheck: false,
-    }),
-  });
-
-  const result = await resp.json();
-
-  allTasks.push(result);
-  valueInput = "";
-  input.value = "";
-  render();
-};
-
-const render = () => {
-  const content = document.getElementById("content-page");
-  while (content.firstChild) {
-    content.removeChild(content.firstChild);
-  }
-  allTasks.forEach(({ _id, isCheck, text }) => {
-    const container = document.createElement("div");
-    container.classList = "task-container";
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = isCheck;
-    checkbox.onchange = () => {
-      onChangeCheckBox(_id);
-    };
-
-    const editBlock = document.createElement("div");
-    editBlock.className = "hidden";
-
-    const editNewInput = document.createElement("input");
-    editNewInput.id = `input-${_id}`;
-
-    editNewInput.value = text;
-    editNewInput.onchange = (e) => {
-      newEdittask = e.target.value;
-    };
-    const editButton = document.createElement("button");
-    editButton.innerText = "add";
-    editBlock.id = `edit-${_id}`;
-    editButton.onclick = () => {
-      updateTaskText(_id);
-    };
-    const cancelButton = document.createElement("button");
-    cancelButton.innerText = "cancel";
-    cancelButton.onclick = () => {
-      closeEditBlock(_id);
-    };
-
-    const taskBlock = document.createElement("div");
-    taskBlock.className = `task`;
-    taskBlock.id = `task-${_id}`;
-
-    const textCreater = document.createElement("p");
-    textCreater.innerText = text;
-    textCreater.className = isCheck ? "text-task done-text" : "text-task";
-
-    const openEdit = document.createElement("button");
-    openEdit.onclick = () => {
-      openEditBlock(_id);
-    };
-    const imageEdit = document.createElement("img");
-    imageEdit.src = "img/pencil-fill.svg";
-
-    const deleteButton = document.createElement("button");
-    deleteButton.onclick = () => {
-      onDeleteTask(_id);
-    };
-    const imageDelete = document.createElement("img");
-    imageDelete.src = "img/x-circle-fill.svg";
-
-    taskBlock.appendChild(checkbox);
-    taskBlock.appendChild(textCreater);
-    taskBlock.appendChild(openEdit);
-    taskBlock.appendChild(deleteButton);
-    editBlock.appendChild(editNewInput);
-    editBlock.appendChild(editButton);
-    editBlock.appendChild(cancelButton);
-    openEdit.appendChild(imageEdit);
-    deleteButton.appendChild(imageDelete);
-    container.appendChild(taskBlock);
-    container.appendChild(editBlock);
-    content.appendChild(container);
-  });
 };
 
 const updateValue = (event) => {
-  valueInput = event.target.value;
+  input.value = event.target.value;
 };
 
 const onChangeCheckBox = async (id) => {
   const selectedTask = allTasks.find((item) => item._id === id);
-  const { _id, text, isCheck = !isCheck } = selectedTask;
+  const { _id, isCheck = !isCheck } = selectedTask;
   allTasks[index].isCheck = !allTasks[index].isCheck;
   try {
     const resp = await fetch(`${URL}/updateTask`, {
@@ -134,9 +65,10 @@ const onChangeCheckBox = async (id) => {
       headers: { "Content-type": "application/json;charset=utf-8" },
       body: JSON.stringify({ _id, isCheck }),
     });
+
     const result = await resp.json();
   } catch (error) {
-    console.log(error);
+    alert("Error");
   }
   render();
 };
@@ -176,9 +108,10 @@ const onDeleteTask = async (id) => {
     allTasks = updatedTask;
     render();
   } catch (error) {
-    alert(error);
+    alert("Error");
   }
 };
+
 const updateTaskText = async (id) => {
   const editInput = document.querySelector(`#input-${id}`);
   allTasks.find((item) => item._id === id);
@@ -191,6 +124,7 @@ const updateTaskText = async (id) => {
         _id: id,
       }),
     });
+
     const result = await resp.json();
     allTasks.forEach((element) => {
       if (element._id === id) {
@@ -199,10 +133,85 @@ const updateTaskText = async (id) => {
     });
     render();
     closeEditBlock();
-  } catch (error) {}
+  } catch (error) {
+    
+  }
 };
 
-const doneEditTask = async () => {
-  activatedEditTask = null;
-  render();
+const render = () => {
+  const content = document.getElementById("content-page");
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+  allTasks.forEach(({ _id, isCheck, text }) => {
+    const container = document.createElement("div");
+    container.classList = "task-container";
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = isCheck;
+    checkbox.onchange = () => {
+      onChangeCheckBox(_id);
+    };
+
+    const editBlock = document.createElement("div");
+    editBlock.className = "hidden";
+
+    const editNewInput = document.createElement("input");
+    editNewInput.id = `input-${_id}`;
+
+    editNewInput.value = text;
+    editNewInput.onchange = (e) => {
+      newEditTask = e.target.value;
+    };
+
+    const taskBlock = document.createElement("div");
+    taskBlock.className = `task`;
+    taskBlock.id = `task-${_id}`;
+
+    const textCreater = document.createElement("p");
+    textCreater.innerText = text;
+    textCreater.className = isCheck ? "text-task done-text" : "text-task";
+
+    const openEdit = document.createElement("button");
+    openEdit.onclick = () => {
+      openEditBlock(_id);
+    };
+
+    const imageEdit = document.createElement("img");
+    imageEdit.src = "img/pencil-fill.svg";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.onclick = () => {
+      onDeleteTask(_id);
+    };
+
+    const imageDelete = document.createElement("img");
+    imageDelete.src = "img/x-circle-fill.svg";
+
+    const editButton = document.createElement("button");
+    editButton.innerText = "add";
+    editBlock.id = `edit-${_id}`;
+    editButton.onclick = () => {
+      updateTaskText(_id);
+    };
+
+    const cancelButton = document.createElement("button");
+    cancelButton.innerText = "cancel";
+    cancelButton.onclick = () => {
+      closeEditBlock(_id);
+    };
+
+    taskBlock.appendChild(checkbox);
+    taskBlock.appendChild(textCreater);
+    taskBlock.appendChild(openEdit);
+    taskBlock.appendChild(deleteButton);
+    editBlock.appendChild(editNewInput);
+    editBlock.appendChild(editButton);
+    editBlock.appendChild(cancelButton);
+    openEdit.appendChild(imageEdit);
+    deleteButton.appendChild(imageDelete);
+    container.appendChild(taskBlock);
+    container.appendChild(editBlock);
+    content.appendChild(container);
+  });
 };
